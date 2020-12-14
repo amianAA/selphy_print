amianAA/selphy_print

commit a3e5952e1f9d2be665c836937a2fce32567218d9

lib2245:  First public release of the reverse-engineered S2245 library
Notes:

 * Creates pretty-decent-looking images, but...
 * Likely has (possibly kitten-consuming) bugs
 * Output has *NOT* been compared to official Sinfonia code
 * Very little actual testing so far!

Makefile
@@ -64,10 +64,12 @@ BACKEND_NAME ?= gutenprint5X+usb
 # Libraries:
 LIBS6145_NAME = lib6145/libS6145ImageReProcess.$(LIB_SUFFIX)
 LIBS6145_SOURCES = lib6145/libS6145ImageReProcess.c
+LIBS2245_NAME = lib2245/libS2245ImageReProcess.$(LIB_SUFFIX)
+LIBS2245_SOURCES = lib2245/libS2245ImageReProcess.c
 LIB70X_NAME ?= lib70x/libMitsuD70ImageReProcess.$(LIB_SUFFIX)
 LIB70X_SOURCES = lib70x/libMitsuD70ImageReProcess.c
 
-LIBRARIES = $(LIBS6145_NAME) $(LIB70X_NAME)
+LIBRARIES = $(LIBS6145_NAME) $(LIB70X_NAME) $(LIBS2245_NAME)
 
 # Tools
 CC ?= $(CROSS_COMPILE)gcc
@@ -90,7 +92,8 @@ E=true
 endif
 
 # Flags
-CFLAGS += -Wall -Wextra -Wformat-security -funit-at-a-time -g -Og -D_FORTIFY_SOURCE=2 -D_GNU_SOURCE -std=c99 -D_POSIX_C_SOURCE=200809L # -Wconversion
+CFLAGS += -Wall -Wextra -Wformat-security -funit-at-a-time -g -Og -D_FORTIFY_SOU
+RCE=2 -D_GNU_SOURCE -std=c99 -D_POSIX_C_SOURCE=200809L # -Wconversion
 LDFLAGS += $(shell pkg-config $(PKG_CONFIG_EXTRA) --libs libusb-1.0)
 CPPFLAGS += $(shell pkg-config $(PKG_CONFIG_EXTRA) --cflags libusb-1.0)
 # CPPFLAGS += -DLIBUSB_PRE_1_0_10
@@ -245,5 +248,9 @@ $(LIB70X_NAME):  $(LIB70X_SOURCES:.c=.o)
 	$(Q)$(CC) $(LIBLDFLAGS) -o $@ $^
 
 $(LIBS6145_NAME):  $(LIBS6145_SOURCES:.c=.o)
+	@$(E) "    CCLD  " $@
+	$(Q)$(CC) $(LIBLDFLAGS) -o $@ $^
+
+$(LIBS2245_NAME):  $(LIBS2245_SOURCES:.c=.o)
 	@$(E) "    CCLD  " $@
 	$(Q)$(CC) $(LIBLDFLAGS) -o $@ $^

lib2245/Makefile
@@ -1,0 +1,48 @@
+# Basic stuff
+LIBS2245_NAME ?= S2245ImageReProcess
+
+# Destination directories
+DESTDIR ?=
+LIBDIR ?= $(DESTDIR)/usr/local/lib
+
+# Tools
+CC ?= $(CROSS_COMPILE)gcc
+CPPCHECK ?= cppcheck
+MKDIR ?= mkdir
+INSTALL ?= install
+RM ?= rm
+
+# Flags
+CFLAGS += -Wall -Wextra -g -Os -std=c99 -D_FORTIFY_SOURCE=2 -fPIC --no-strict-overflow # -Wconversion
+#LDFLAGS +=
+#CPPFLAGS +=
+CFLAGS += -funit-at-a-time
+
+ifneq (,$(findstring mingw,$(CC)))
+SUFFIX=dll
+else
+SUFFIX=so
+endif
+
+# Build stuff
+SOURCES = libS2245ImageReProcess.c
+
+# And now the rules!
+.PHONY: clean all install cppcheck
+
+all: lib$(LIBS2245_NAME).$(SUFFIX)
+
+cppcheck:
+	$(CPPCHECK) -q -v --std=c99 --enable=all -I/usr/include $(CPPFLAGS) $(SOURCES)
+
+install:
+	$(MKDIR) -p $(LIBDIR)
+	$(INSTALL) -o root -m 755 lib$(LIBS2245_NAME).$(SUFFIX) $(LIBDIR)
+clean:
+	$(RM) -f lib$(LIBS6145_NAME).$(SUFFIX) *.o
+
+lib$(LIBS2245_NAME).$(SUFFIX):  $(SOURCES:.c=.o)
+	$(CC) $(LDFLAGS) -g -shared -o $@ $^
+
+%.o: %.c
+	$(CC) -c $(CFLAGS) -o $@ $<

commit 4208c225210f24f9d40aa04314886bdb2bdfc6e3

Fix CFLAGS.  It was broken in a3e5952e1f9d2be665c836937a2fce32567218d9

Makefile
@@ -92,8 +92,7 @@ E=true
 endif
 
 # Flags
-CFLAGS += -Wall -Wextra -Wformat-security -funit-at-a-time -g -Og -D_FORTIFY_SOU
-RCE=2 -D_GNU_SOURCE -std=c99 -D_POSIX_C_SOURCE=200809L # -Wconversion
+CFLAGS += -Wall -Wextra -Wformat-security -funit-at-a-time -g -Og -D_FORTIFY_SOURCE=2 -D_GNU_SOURCE -std=c99 -D_POSIX_C_SOURCE=200809L # -Wconversion
 LDFLAGS += $(shell pkg-config $(PKG_CONFIG_EXTRA) --libs libusb-1.0)
 CPPFLAGS += $(shell pkg-config $(PKG_CONFIG_EXTRA) --cflags libusb-1.0)
 # CPPFLAGS += -DLIBUSB_PRE_1_0_10