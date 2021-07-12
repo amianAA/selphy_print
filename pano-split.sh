45f4f06b3c2a30ef14f3393aaef2b975d4e7d730
Solomon Peachy <pizza@shaftnet.org>
2021-07-12 09:48:10 -0400

    Panorama:  Add definitions for Kodak 8810 and 6900/6950 in split script

--- pano-split.sh
+++ pano-split.sh
@@ -5,7 +5,10 @@
 #
 # Note that this is _very_ preliminary, doing the fading/overlapping in a
 # very naive linear manner.
-
+#
+# Also note that this script will probably go away as its functionality
+# is moved into selphy_print &| gutenprint
+#
 infile="$1"
 outfile="$2"
 model="$3"
@@ -19,6 +22,67 @@ fi
 
 # D90
 case "${model}" in
+    kodak-6900|kodak-6950)
+	overlap=600 # XXX Not sure yet
+	if [ "${printsize}" eq "6x20" ] ; then
+	    cols=1844
+	    inrows=6108 # XXX Not sure
+	    drows=2436
+	elif [ "${printsize}" eq "6x18" ] ; then
+	    cols=1844
+	    inrows=5508 # XXX Not sure
+	    drows=2436
+	elif [ "${printsize}" eq "6x14" ] ; then
+	    cols=1844
+	    inrows=4308 # XXX Not sure
+	    drows=2436
+	elif [ "${printsize}" eq "6x12" ] ; then
+	    cols=1844
+	    inrows=3708 # XXX Not sure
+	    drows=2436
+	elif [ "${printsize}" eq "5x20" ] ; then
+	    cols=1548
+	    inrows=6108 # XXX Not sure
+            overlap=150 # XXX Not sure
+	    drows=2136
+	elif [ "${printsize}" eq "5x15" ] ; then
+	    cols=1548
+	    inrows=4608 # XXX Not sure
+	    drows=2136
+	elif [ "${printsize}" eq "5x10" ] ; then
+	    cols=1548
+	    inrows=3108 # XXX Not sure
+	    drows=2136
+	else
+	    echo "EK69xx supports 6x20, 6x18, 6x14, 6x12, 5x20, 5x15, and 5x10 only";
+	fi
+    ;;
+    kodak-8810)
+	cols=2464
+	overlap=360
+	if [ "${printsize}" eq "8x14" ] ; then
+	    inrows=4224
+	    drows=3024 # or 3624
+	elif [ "${printsize}" eq "8x16" ] ; then
+	    inrows=4824
+	    drows=3024 # or 3624
+	elif [ "${printsize}" eq "8x20" ] ; then
+	    inrows=6024
+	    drows=3024 # or 3624?
+	elif [ "${printsize}" eq "8x24" ] ; then
+	    inrows=7224
+	    drows=3024 # or 3624, with overlap=180
+	elif [ "${printsize}" eq "8x32" ] ; then
+	    inrows=9624
+	    drows=3624
+	elif [ "${printsize}" eq "8x36" ] ; then
+	    overlap=180
+	    inrows=10824
+	    drows=3624
+	else
+	    echo "EK8810 supports 8x14, 8x16, 8x20, 8x32, and 8x36 only";
+	fi
+    ;;
     mitsubishi-d90dw)
 	cols=1852
 	drows=2428
